df44b0dff403407d04cc4cdcb892a8d694a39506
GarikGelios <garikgelios@gmail.com>
2020-05-03 21:44:48 +0300

    lesson 2 Directives

---
+++ script.js
@@ -0,0 +1,12 @@
+new Vue ({
+    el: '#app',
+    data: {
+        message: 'Hello world',
+        name: '',
+        list: '<ul><li>I`am ok!</li><li>Happy!!</li><li>Amazing!!!</li></ul>',
+        display: true,
+        mymood1: 'I`am ok!',
+        mymood2: 'Happy!!',
+        mymood3: 'Amazing!!!'
+    }
+})
